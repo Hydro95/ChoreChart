bfc9dfffd53eb73fb9bb0255ec15abc27b63f14a
Jonathan <DudeRandom21@gmail.com>
2017-11-24 17:56:03 -0500

    implemented task fragment

---
+++ Source/app/src/main/java/net/sudormrf/chorechart/TaskArrayAdapter.java
@@ -0,0 +1,45 @@
+package net.sudormrf.chorechart;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jon on 24/11/17.
+ */
+
+//Code adapted from github wiki "Using an ArrayAdapter with ListView"
+//at link: https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+public class TaskArrayAdapter extends ArrayAdapter<Task> {
+
+    TaskArrayAdapter(Context context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Task task = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.task_layout, parent, false);
+        }
+        // Lookup view for data population
+        ImageView icon = (ImageView) convertView.findViewById(R.id.taskIcon);
+        TextView name = (TextView) convertView.findViewById(R.id.taskName);
+        TextView deadline = (TextView) convertView.findViewById(R.id.deadline);
+        // Populate the data into the template view using the data object
+        icon.setImageResource(task.icon);
+        name.setText(task.name);
+        deadline.setText("Deadline:" + task.deadline);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
