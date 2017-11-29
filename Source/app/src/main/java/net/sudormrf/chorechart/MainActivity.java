d462c0aedb1862245ef7079cffa35c42774a07e7
Josh Vinge <joshvinge@outlook.com>
2017-11-28 20:21:24 -0500

    remove useless imports

--- Source/app/src/main/java/net/sudormrf/chorechart/MainActivity.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/MainActivity.java
@@ -2,8 +2,6 @@ package net.sudormrf.chorechart;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -11,14 +9,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
