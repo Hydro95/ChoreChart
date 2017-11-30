98b06053189e3c395afc6270d6b8fb495e01c1e1
Walter Wong <32001362+Walt280@users.noreply.github.com>
2017-11-30 01:31:54 -0500

    Finishing touches on UI: - Add arbitary image support. - Add framework for task serialization.

--- Source/app/src/main/java/net/sudormrf/chorechart/EditTaskActivity.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/EditTaskActivity.java
@@ -1,12 +1,21 @@
 package net.sudormrf.chorechart;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.util.Calendar;
 import java.util.List;
 
@@ -15,11 +24,15 @@ public class EditTaskActivity extends AppCompatActivity implements
         DatePickerFragment.OnDateSetListener,
         TimePickerFragment.OnTimeSetListener {
 
+    private static final int PICK_IMAGE = 1;
+    private static final int CROP_IMAGE = 2;
+
     //Date stored as variable to facilitate getting data from the fragment.
     private Calendar deadline;
 
     private boolean dateSet;
     private boolean timeSet;
+    private boolean isNewTask;
 
     @Override
     //TODO: Add a way to tell this activity,whether its a new task or an existing one.
@@ -58,6 +71,64 @@ public class EditTaskActivity extends AppCompatActivity implements
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        System.out.println(id);
+
+        //TODO: Add delete confirm alertdialog.
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            System.out.println("trash");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSaveButtonClick(View view)
+    {
+        //TODO: Implement db sync.
+        //TODO: Bitmap to Base64.
+        finish();
+    }
+
+    //Launching an intent to get a image from gallery based on
+    //https://stackoverflow.com/questions/5309190/android-pick-images-from-gallery
+    public void onIconClick(View view)
+    {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        //Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        //chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(pickIntent, PICK_IMAGE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+            if(resultCode == RESULT_OK) {
+                cropImage(data.getData());
+            }
+        }
+        else if(requestCode == CROP_IMAGE) {
+            if(resultCode == RESULT_OK) {
+                ImageView img = (ImageView) findViewById(R.id.task_image);
+                Bundle imgData = data.getExtras();
+                Bitmap stuff = imgData.getParcelable("data");
+                img.setImageBitmap(stuff);
+            }
+        }
+    }
     public void onDateTimeSet(Calendar datetime) { }
 
     public void onDateSet(Calendar date)
@@ -85,4 +156,21 @@ public class EditTaskActivity extends AppCompatActivity implements
             dt.updateDateTime(deadline);
         }
     }
+
+    //Image cropping intent based off of:
+    //https://code.tutsplus.com/tutorials/capture-and-crop-an-image-with-the-device-camera--mobile-11458
+    private void cropImage(Uri imgUri)
+    {
+        //Uri selImage = data.getData();
+        Intent cropIntent = new Intent("com.android.camera.action.CROP");
+        cropIntent.setDataAndType(imgUri, "image/*");
+        cropIntent.putExtra("crop", "true");
+        cropIntent.putExtra("aspectX", 1);
+        cropIntent.putExtra("aspectY", 1);
+        cropIntent.putExtra("outputX", 128);
+        cropIntent.putExtra("outputY", 128);
+        cropIntent.putExtra("return-data", true);
+        startActivityForResult(cropIntent, CROP_IMAGE);
+    }
+
 }
