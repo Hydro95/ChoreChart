f2868cd056d4b1314391344b0dee28db00f4c60e
Jonathan <DudeRandom21@gmail.com>
2017-11-30 18:32:51 -0500

    fixed minor issues with previous commit

--- Source/app/src/main/java/net/sudormrf/chorechart/UserModifierActivity.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/UserModifierActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -18,9 +17,6 @@ import android.widget.Toast;
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.io.IOException;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -31,7 +27,7 @@ public class UserModifierActivity extends AppCompatActivity {
     private Uri mImageUri;
     private Bitmap userImg;
 
-    User user;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
