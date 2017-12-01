135d7a9255e60d71dcd0dcfbdc41b9f35df4d452
Jonathan <DudeRandom21@gmail.com>
2017-12-01 17:16:57 -0500

    added add item functionallity to item list

--- Source/app/src/main/java/net/sudormrf/chorechart/ItemList.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/ItemList.java
@@ -12,13 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class ItemList extends AppCompatActivity {
+
+    ShoppingList shoppingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        int index = getIntent().getIntExtra("index", -1);
+
+        shoppingList = Facade.getInstance().getShoppingList(index);
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
@@ -37,5 +45,14 @@ public class ItemList extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public void onAddButtonClick(View view) {
+
+        EditText item = findViewById(R.id.itemName);
+
+        shoppingList.addItem(item.getText().toString());
+
+        Facade.getInstance().publishShoppingLists();
     }
 }
