fe638c6e9be30793a55dc5d888e169d31c78ce13
Walter Wong <32001362+Walt280@users.noreply.github.com>
2017-12-01 17:57:08 -0500

    Clean up some files.

--- Source/app/src/main/java/net/sudormrf/chorechart/DateTimeFragment.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/DateTimeFragment.java
@@ -1,21 +1,17 @@
 package net.sudormrf.chorechart;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,14 +22,6 @@ import java.util.Calendar;
  * create an instance of this fragment.
  */
 public class DateTimeFragment extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnDateTimeSetListener mListener;
 
@@ -45,16 +33,11 @@ public class DateTimeFragment extends Fragment implements View.OnClickListener {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment DateTimeFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static DateTimeFragment newInstance(String param1, String param2) {
+    public static DateTimeFragment newInstance() {
         DateTimeFragment fragment = new DateTimeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,10 +45,6 @@ public class DateTimeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
