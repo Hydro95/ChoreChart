aafb151053e88827b9bea40beb1c24ac527de02b
Josh Vinge <joshvinge@outlook.com>
2017-11-30 21:44:23 -0500

    integrated editing and creating users

--- Source/app/src/main/java/net/sudormrf/chorechart/PeopleFragment.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/PeopleFragment.java
@@ -27,15 +27,6 @@ public class PeopleFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_people, container, false);
-        //generate some dummy users for the UI
-        /*
-        for (int i = 0; i < 5; i++) {
-            String name = "Anon" + Integer.toString((int)Math.round(Math.random() * 99));
-            User nextUser = Facade.getInstance().addUser(name, R.drawable.ic_logo_mil);
-            nextUser.setPoints((int)Math.round(Math.random() * 99));
-        }
-        Facade.getInstance().publishUsers();
-        */
 
         UserArrayAdapter adapter = new UserArrayAdapter(getActivity(), Facade.getInstance().getUsers());
 
