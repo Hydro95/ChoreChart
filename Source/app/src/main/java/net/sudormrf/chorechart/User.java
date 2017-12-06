c13b7e457f1986f12ec18b2f53acb65207cbbd26
Ryan Ethier <rethi061@uottawa.ca>
2017-12-06 11:48:29 -0500

    Re-instating Ryan Branch

--- Source/app/src/main/java/net/sudormrf/chorechart/User.java
+++ Source/app/src/main/java/net/sudormrf/chorechart/User.java
@@ -108,6 +108,12 @@ public class User
     return aTaskId;
   }
 
+  public String[] getTaskIds()
+  {
+    String[] newTaskIds = taskIds.toArray(new String[taskIds.size()]);
+    return newTaskIds;
+  }
+
   public int numberOfTaskIds()
   {
     int number = taskIds.size();
@@ -130,9 +136,9 @@ public class User
   {}
 
   // line 23 "../../../class.ump"
-   public List<String> getTaskIds(){
-    return taskIds;
-  }
+   //public List<String> getTaskIds(){
+   // return taskIds;
+  //}
 
 
   public String toString()
